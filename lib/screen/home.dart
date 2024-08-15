be0aef37f77b2360a63c76ce5c565dbc556edce5
soorya <sooryasivask@gmail.com>
2024-08-15 21:20:18 +0530

    home ui

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -56,20 +56,18 @@ class _HomeState extends State<Home> {
                 Row(
                   children: [
                     wspace(5),
-                    Builder(
-                      builder: (context) {
-                        return IconButton(
-                          onPressed: () {
-                            Scaffold.of(context).openDrawer();
-                          },
-                          icon: Icon(
-                            IconlyBold.category,
-                            color: cr_sec,
-                            size: 15.r,
-                          ),
-                        );
-                      }
-                    ),
+                    Builder(builder: (context) {
+                      return IconButton(
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                        icon: Icon(
+                          IconlyBold.category,
+                          color: cr_sec,
+                          size: 15.r,
+                        ),
+                      );
+                    }),
                     wspace(5),
                     Text(
                       'Vaultify',
@@ -92,7 +90,8 @@ class _HomeState extends State<Home> {
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Text(
-                                "No records. tap lock icon to add new ",
+                                "No records.\nTap lock icon to add new ",
+                                textAlign: TextAlign.center,
                                 style: TextStyle(
                                   color: cr_sec,
                                   fontWeight: FontWeight.bold,
@@ -116,121 +115,8 @@ class _HomeState extends State<Home> {
                           itemCount: plist.length,
                           physics: const BouncingScrollPhysics(),
                           itemBuilder: (context, index) {
-                            Vault item = plist.elementAt(index);
-                            bool show = false;
-                            return Slidable(
-                              startActionPane: ActionPane(
-                                extentRatio: 0.2,
-                                motion: const ScrollMotion(),
-                                children: [
-                                  MaterialButton(
-                                    color: Colors.blue.withOpacity(0.15),
-                                    elevation: 0,
-                                    shape: const CircleBorder(),
-                                    child: Padding(
-                                      padding: EdgeInsets.all(5.r),
-                                      child: Icon(
-                                        Icons.edit,
-                                        color: Colors.blue,
-                                        size: 20.r,
-                                      ),
-                                    ),
-                                    onPressed: () {
-                                      route(
-                                          context,
-                                          Newpass(
-                                            edit: true,
-                                            vault: item,
-                                          ));
-                                    },
-                                  ),
-                                ],
-                              ),
-                              endActionPane: ActionPane(
-                                extentRatio: 0.2,
-                                motion: const ScrollMotion(),
-                                children: [
-                                  MaterialButton(
-                                    color: Colors.redAccent.withOpacity(0.15),
-                                    elevation: 0,
-                                    shape: const CircleBorder(),
-                                    child: Padding(
-                                      padding: EdgeInsets.all(5.r),
-                                      child: Icon(
-                                        IconlyBold.delete,
-                                        color: Colors.redAccent,
-                                        size: 20.r,
-                                      ),
-                                    ),
-                                    onPressed: () {
-                                      setState(() {
-                                        VaultHelper().delvault(item.id!);
-                                      });
-                                    },
-                                  ),
-                                ],
-                              ),
-                              child: InkWell(
-                                onTap: () {
-                                  route(context,
-                                      Newpass(edit: true, vault: item));
-                                },
-                                child: Padding(
-                                  padding: EdgeInsets.symmetric(
-                                      vertical: 10.h, horizontal: 10.w),
-                                  child: GlassCard(
-                                    heigth: 70.h,
-                                    width: size.width,
-                                    color: cr_sec,
-                                    child: Row(
-                                      children: [
-                                        wspace(5),
-                                        CircleAvatar(
-                                          radius: 20.sp,
-                                          backgroundColor: cr_pri,
-                                          child: Icon(
-                                            IconlyBold.lock,
-                                            size: 15.r,
-                                            color: cr_sec.withOpacity(0.8),
-                                          ),
-                                        ),
-                                        Expanded(
-                                          child: ListTile(
-                                            title: Text(
-                                              item.site.toUpperCase(),
-                                              style: TextStyle(
-                                                fontSize: 12.sp,
-                                                color: cr_blk,
-                                              ),
-                                            ),
-                                            subtitle: Text(
-                                              item.username,
-                                              style: TextStyle(
-                                                fontSize: 10.sp,
-                                                color: cr_grey,
-                                              ),
-                                            ),
-                                            trailing: IconButton(
-                                                onPressed: () async {
-                                                  Clipboard.setData(ClipboardData(
-                                                      text: item.password));
-                                                  Fluttertoast.showToast(
-                                                      msg:
-                                                      'Password Copied to clipboard');
-                                                },
-                                                icon: Icon(
-                                                  FontAwesomeIcons.copy,
-                                                  color: cr_secac,
-                                                  size: 12.sp,
-                                                )),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            );
+                            Vault vault = plist.elementAt(index);
+                            return VaultTile(vault: vault);
                           },
                         ),
                       );
@@ -308,6 +194,142 @@ class _HomeState extends State<Home> {
           ),
         ),*/
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      ),
+    );
+  }
+}
+
+class VaultTile extends StatefulWidget {
+  VaultTile({super.key, required this.vault});
+
+  Vault vault;
+
+  @override
+  State<VaultTile> createState() => _VaultTileState();
+}
+
+class _VaultTileState extends State<VaultTile> {
+  bool show = false;
+  late Vault vault;
+  @override
+  Widget build(BuildContext context) {
+    vault = widget.vault;
+    return Slidable(
+      startActionPane: ActionPane(
+        extentRatio: 0.2,
+        motion: const ScrollMotion(),
+        children: [
+          MaterialButton(
+            color: Colors.blue.withOpacity(0.15),
+            elevation: 0,
+            shape: const CircleBorder(),
+            child: Padding(
+              padding: EdgeInsets.all(5.r),
+              child: Icon(
+                Icons.edit,
+                color: Colors.blue,
+                size: 20.r,
+              ),
+            ),
+            onPressed: () {
+              route(
+                  context,
+                  Newpass(
+                    edit: true,
+                    vault: vault,
+                  ));
+            },
+          ),
+        ],
+      ),
+      endActionPane: ActionPane(
+        extentRatio: 0.2,
+        motion: const ScrollMotion(),
+        children: [
+          MaterialButton(
+            color: Colors.redAccent.withOpacity(0.15),
+            elevation: 0,
+            shape: const CircleBorder(),
+            child: Padding(
+              padding: EdgeInsets.all(5.r),
+              child: Icon(
+                IconlyBold.delete,
+                color: Colors.redAccent,
+                size: 20.r,
+              ),
+            ),
+            onPressed: () {
+              setState(() {
+                VaultHelper().delvault(vault.id!);
+              });
+            },
+          ),
+        ],
+      ),
+      child: InkWell(
+        onTap: () {
+          route(context, Newpass(edit: true, vault: vault));
+        },
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
+          child: GlassCard(
+            heigth: 70.h,
+            width: double.maxFinite,
+            color: cr_sec,
+            child: Row(
+              children: [
+                wspace(5),
+                CircleAvatar(
+                  radius: 20.sp,
+                  backgroundColor: cr_pri,
+                  child: InkWell(
+                    onTap: () {
+                      setState(() {
+                        show = !show;
+                      });
+                    },
+                    child: Icon(
+                      show ? Icons.lock_open : IconlyBold.lock,
+                      size: 15.r,
+                      color: cr_sec.withOpacity(0.8),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListTile(
+                    title: Text(
+                      vault.site,
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        color: cr_blk,
+                      ),
+                    ),
+                    subtitle: Text(
+                      show ? vault.username : '********',
+                      style: TextStyle(
+                        fontSize: 10.sp,
+                        color: cr_grey,
+                      ),
+                    ),
+                    trailing: IconButton(
+                        onPressed: () async {
+                          Clipboard.setData(
+                              ClipboardData(text: vault.password));
+                          Fluttertoast.showToast(
+                            msg: 'Password Copied to clipboard',
+                          );
+                        },
+                        icon: Icon(
+                          FontAwesomeIcons.copy,
+                          color: cr_secac,
+                          size: 12.sp,
+                        )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
